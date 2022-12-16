79a636166e5d4971ab0fb49cdb34675d060061ab
Lucas Alves <lucasvelosoalves@outlook.com>
2022-12-16 10:28:25 -0300

    Lucas def delete

--- db/conn.py
+++ db/conn.py
@@ -1,5 +1,5 @@
 from config import create_log
-from .crud import select
+from .crud import select, delete
 import tkinter as tk
 from tkinter import *
 #Ttk treeview não vem com Tkinter padrão
@@ -17,11 +17,6 @@ def access(dbuser, dbpass):
     password = dbpass 
   )
 
-  # 1- abrindo conexão
-  # 2- o creat_log cria o banco
-  # 3- o execute, executa a variavel
-  # 4- o "Use" seleciona o banco
-  # 5- o segundo execute,executa a variavel
   create_log.logFile('Conectando...')
   try:
     cursor = cnx.cursor()
@@ -35,7 +30,8 @@ def access(dbuser, dbpass):
 
  #Select por tabela escolhida
     try:
-     print('Tabela select')
+     print('Veja dados ')
+     print('Tecle enter para pular')
      tabela = str(input('Digite tabela: '))
      coluna = str(input('Digite coluna: '))
      condicao = str(input('Qual o campo? (tecle enter para cancelar): '))
@@ -46,70 +42,17 @@ def access(dbuser, dbpass):
       print('Erro ao chamar funcao da crud.')
 
 
-    # #Select tabela arbitro
-    # try:
-    #   select.selectArbitro(cursor,'','')
-    #   select.selectArbitro(cursor,'Salima Mukansanga','')
-    #   select.selectArbitro(cursor,'',8)
-    # except Exception as error:
-    #   create_log.logFile(f'Erro ao chamar funcao da crud.\n{error}')
-    #   print('Erro ao chamar funcao da crud.')
-
-    # #Select tabela tecnico
-    # try:
-    #   select.selectTecnico(cursor,'','')
-    #   select.selectTecnico(cursor,'Paulo Bento','')
-    #   select.selectTecnico(cursor,'',25)
-    #   select.selectTecnico(cursor,'','')
-    # except Exception as error:
-    #   create_log.logFile(f'Erro ao chamar funcao da crud.\n{error}')
-    #   print('Erro ao chamar funcao da crud.')
-
-    # #Select tabela equipe
-    # try:
-    #     select.selectEquipe(cursor,'','')
-    #     select.selectEquipe(cursor,'','')
-    #     select.selectEquipe(cursor,'','')
-    # except Exception as error:
-    #   create_log.logFile(f'Erro ao chamar funcao da crud.\n{error}')
-    #   print('Erro ao chamar funcao da crud.')
-    # # Select tabela fase
-    # try:
-    #   select.selectFase(cursor,'','')
-    #   select.selectFase(cursor,'','')
-    #   select.selectFase(cursor,'','')
-    #   select.selectFase(cursor,'','')
-    # except Exception as error:
-    #   create_log.logFile(f'Erro ao chamar funcao da crud.\n{error}')
-    #   print('Erro ao chamar funcao da crud.')
-    #  # Select tabela grupo
-    # try:
-    #   select.selectGrupo(cursor,'','')
-    #   select.selectGrupo(cursor,'','')
-    #   select.selectGrupo(cursor,'','')
-    #   select.selectGrupo(cursor,'','')
-    # except Exception as error:
-    #   create_log.logFile(f'Erro ao chamar funcao da crud.\n{error}')
-    #   print('Erro ao chamar funcao da crud.')
-
-    # #Seletc tabela partidas
-    # try:
-    #   select.selectPartidas(cursor,'','')
-    #   select.selectPartidas(cursor,'','')
-    #   select.selectPartidas(cursor,'','')
-    #   select.selectPartidas(cursor,'','')
-    #   select.selectPartidas(cursor,'','')
-    #   select.selectPartidas(cursor,'','')
-    #   select.selectPartidas(cursor,'','')
-    #   select.selectPartidas(cursor,'','')
-    #   select.selectPartidas(cursor,'','')
-    #   select.selectPartidas(cursor,'','')
-    # except Exception as error:
-    #   create_log.logFile(f'Erro ao chamar funcao da crud.\n{error}')
-    #   print('Erro ao chamar funcao da crud.')
-
   #Delete tabela por tabela escolhida
-
+    try:
+     print('Delete dados')
+     print('Tecle enter para pular')
+     tabela = str(input('Digite tabela: '))
+     coluna = str(input('Digite coluna: '))
+     condicao = str(input('Digite o id referente ao dado que deseja exluir: '))
+     delete.deletar(cnx,cursor,tabela,coluna,condicao)
+    except Exception as error:
+      create_log.logFile(f'Erro ao chamar funcao da crud.\n{error}')
+      print('Erro ao chamar funcao da crud.')
   #Insert tabela por tabela escolhida
     
 
