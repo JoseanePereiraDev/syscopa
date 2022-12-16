33ed1091aab0e812038370926fc4e0f57cbc81b2
Vinícius Silva <viniciusdsnascimento@gmail.com>
2022-12-16 20:22:22 -0300

    Vinícius corrigido

--- db/crud/insert.py
+++ db/crud/insert.py
@@ -2,9 +2,12 @@ import datetime
 #Insert
 def inserir(cnx,cursor,tabela,coluna,valores):
   if valores != '':
-    cursor.execute(f'insert into {tabela} ({coluna}) values "({valores})" ;')
-    cnx.commit()
-    print(f'{cursor.rowcount}, Linhas inseridas')
-    print(cursor.fetchall())#fetchal trás tudo
+    try:
+      cursor.execute(f'insert into {tabela} ({coluna}) values "({valores})" ;')
+      cnx.commit()
+      print(f'{cursor.rowcount}, Linhas inseridas')
+      print(cursor.fetchall())#fetchal trás tudo
+    except Exception as error:
+      print(f"Erro def inserir: {error}")
   else:
     print("Tabela esta vazia!")##tabela não está vazia, usar o try except igual as outras
